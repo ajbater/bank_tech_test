0f5643359311c4dbf894bf8aa2e22a41b9d63e6d
ajbater <amanda22@hotmail.co.uk>
2017-01-23 11:07:47 +0000

    transaction knows what type of transaction it is

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -20,4 +20,9 @@ describe('Transaction', function() {
     var newTransaction = new Transaction('withdraw', 500, '10-01-2012');
     expect(newTransaction._amount).toEqual(500);
   });
+
+  it('has a type, which will be passed upon instantiation', function() {
+    var newTransaction = new Transaction('withdraw', 500, '10-01-2012');
+    expect(newTransaction._type).toEqual('withdraw');
+  });
 });
