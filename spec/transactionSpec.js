8ff9fd8c7463c0df5398776aa803b701c7fdf78e
ajbater <amanda22@hotmail.co.uk>
2017-01-23 20:03:57 +0000

    transaction knows what the updated balance is after money has been debited or credited

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -19,13 +19,13 @@ describe('Transaction', function() {
     expect(transaction._amountDebited).toBe(null);
   });
 
+  it('has a new balance property which gives the updated balance after each transaction', function() {
+    expect(transaction._newBalance).toEqual(500);
+  });
+
   // it('has a method that formats the date into a string', function() {
   //   var date = new Date('01-23-2017');
   //   jasmine.clock().mockDate(date);
   //   expect(transaction.formatDate()).toEqual('23/01/2017');
   // });
-
-  it('has a new balance property', function() {
-    expect('_newBalance' in transaction).toBe(true);
-  });
 });
