571d7144c99172d3242b1a45926b516e94fbf182
ajbater <amanda22@hotmail.co.uk>
2017-01-23 19:57:53 +0000

    refactoring - account now only responsible for balance

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -11,66 +11,62 @@ describe('Account', function() {
     expect(account._balance).toEqual(0);
   });
 
-  it('has the capacity to store transactions', function() {
-    expect(account._transactionHistory).toEqual([]);
-  });
+  // it('has the capacity to store transactions', function() {
+  //   expect(account._transactionHistory).toEqual([]);
+  // });
 
-  describe('depositing funds', function() {
-    beforeEach(function() {
-      account.deposit(500);
-    });
+  // describe('depositing funds', function() {
+  //   beforeEach(function() {
+  //     account.deposit(500);
+  //   });
+  //
+  //   it('has a deposit function which adds the amount passed in to the account balance', function() {
+  //     expect(account._balance).toEqual(500.00)
+  //   });
+  //
+  //   it('making a deposit will add a new transaction to the transaction history', function() {
+  //     expect(account._transactionHistory.length).toEqual(1);
+  //   });
+  //
+  //   it('making a deposit will create a new transaction object', function() {
+  //     expect(account._transactionHistory[0]).toEqual(jasmine.any(Transaction))
+  //   });
+  //
+  //   it('making a deposit will pass the new balance to the transaction object', function() {
+  //     expect(account._transactionHistory[0]._newBalance).toEqual('500.00');
+  //   });
+  // });
+  //
+  // describe('withdrawing funds', function() {
+  //   beforeEach(function() {
+  //     account.withdraw(500);
+  //   });
+  //
+  //   it('has a withdraw function which deducts the amount passed in from the account balance', function() {
+  //     expect(account._balance).toEqual(-500.00);
+  //   });
+  //
+  //   it('making a withdrawal will add a new transaction to the transaction history', function() {
+  //     expect(account._transactionHistory.length).toEqual(1);
+  //   });
+  //
+  //   it('making a withdrawal will create a new transaction object', function() {
+  //     expect(account._transactionHistory[0]).toEqual(jasmine.any(Transaction))
+  //   });
+  //
+  //   it('making a withdrawl will pass the new balance to the transaction object', function() {
+  //     expect(account._transactionHistory[0]._newBalance).toEqual('-500.00');
+  //   });
+  // });
 
-    it('has a deposit function which adds the amount passed in to the account balance', function() {
-      expect(account._balance).toEqual(500.00)
-    });
-
-    it('making a deposit will add a new transaction to the transaction history', function() {
-      expect(account._transactionHistory.length).toEqual(1);
-    });
-
-    it('making a deposit will create a new transaction object', function() {
-      expect(account._transactionHistory[0]).toEqual(jasmine.any(Transaction))
-    });
-
-    it('making a deposit will pass the new balance to the transaction object', function() {
-      expect(account._transactionHistory[0]._newBalance).toEqual('500.00');
-    });
-  });
-
-  describe('withdrawing funds', function() {
-    beforeEach(function() {
-      account.withdraw(500);
-    });
-
-    it('has a withdraw function which deducts the amount passed in from the account balance', function() {
-      expect(account._balance).toEqual(-500.00);
-    });
-
-    it('making a withdrawal will add a new transaction to the transaction history', function() {
-      expect(account._transactionHistory.length).toEqual(1);
-    });
-
-    it('making a withdrawal will create a new transaction object', function() {
-      expect(account._transactionHistory[0]).toEqual(jasmine.any(Transaction))
-    });
-
-    it('making a withdrawl will pass the new balance to the transaction object', function() {
-      expect(account._transactionHistory[0]._newBalance).toEqual('-500.00');
-    });
-  });
-
-  describe('requesting the statement', function() {
-    beforeEach(function() {
-      account.deposit(500);
-      account.withdraw(50);
-    });
-
-    it('creates a new statement when you request one', function() {
-      expect(account._statement).toEqual(jasmine.any(Statement));
-    });
-
-    // it('can add each transaction to the statement', function() {
-    //   expect(account._statement[0]).toEqual(['23/01/2016', '500.00', undefined, '500.00']);
-    // });
-  });
+  // describe('requesting the statement', function() {
+  //   beforeEach(function() {
+  //     account.deposit(500);
+  //     account.withdraw(50);
+  //   });
+  //
+  //   it('creates a new statement when you request one', function() {
+  //     expect(account._statement).toEqual(jasmine.any(Statement));
+  //   });
+  // });
 });
--- src/account.js
+++ src/account.js
@@ -1,35 +1,23 @@
+// responsible for: managing the balance
 function Account() {
   this._balance = 0.00;
-  this._transactionHistory = [];
   this._statement = new Statement();
 };
 
-Account.prototype.deposit = function(amount) {
-  this._balance += amount;
-  var type = 'deposit'
-  this.createTransaction(type, amount);
-};
+// Account.prototype.deposit = function(amount) {
+//   this._balance += amount;
+//   var type = 'deposit'
+//   this.createTransaction(type, amount);
+// };
+//
+// Account.prototype.withdraw = function(amount) {
+//   this._balance -= amount;
+//   var type = 'withdraw';
+//   this.createTransaction(type, amount);
+// };
 
-Account.prototype.withdraw = function(amount) {
-  this._balance -= amount;
-  var type = 'withdraw';
-  this.createTransaction(type, amount);
-};
-
-Account.prototype.createTransaction = function(type, amount) {
-  var newBalance = this._balance;
-  var transaction = new Transaction(type, amount, newBalance);
-  this._transactionHistory.push(transaction);
-};
-// 
-// Account.prototype.addTransactionToStatement = function() {
-//   for (i = 0; i < this._transactionHistory.length; i++) {
-//     if (this._transactionHistory[i]._type === 'deposit') {
-//       var transaction = [(this._transactionHistory[i]._date.formatDate()), (this._transactionHistory[i]._amount), undefined, (this._transactionHistory[i]._newBalance)];
-//       this._statement._transactions.push(transaction);
-//     } else {
-//       var transaction = [(this._transactionHistory[i]._date.formatDate()), undefined, (this._transactionHistory[i]._amount), (this._transactionHistory[i]._newBalance)];
-//       this._statement._transactions.push(transaction);
-//     }
-//   }
+// Account.prototype.createTransaction = function(type, amount) {
+//   var newBalance = this._balance;
+//   var transaction = new Transaction(type, amount, newBalance);
+//   this._transactionHistory.push(transaction);
 // };
