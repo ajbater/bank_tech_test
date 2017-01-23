afd399777dda0b36601c04bec1dc41724070863a
ajbater <amanda22@hotmail.co.uk>
2017-01-23 20:00:14 +0000

    refactoring transaction - now has amount credited and amount debited

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -4,26 +4,26 @@ describe('Transaction', function() {
   var transaction;
 
   beforeEach(function() {
-    transaction = new Transaction('withdraw', 500, 500);
-  });
-
-  it('has a type, which will be passed upon instantiation', function() {
-    expect(transaction._type).toEqual('withdraw');
-  });
-
-  it('has an amount, which will be an integer', function() {
-    expect(transaction._amount).toEqual('500.00');
+    transaction = new Transaction(500.00, null, 500);
   });
 
   it('has a date, which will be a date object', function() {
     expect(transaction._date).toEqual(jasmine.any(Date));
   });
 
-  it('has a method that formats the date into a string', function() {
-    var date = new Date('01-23-2017');
-    jasmine.clock().mockDate(date);
-    expect(transaction.formatDate()).toEqual('23/01/2017');
+  it('has an amount credited', function() {
+    expect(transaction._amountCredited).toEqual(500.00);
   });
+
+  it('has an amount debited', function() {
+    expect(transaction._amountDebited).toBe(null);
+  });
+
+  // it('has a method that formats the date into a string', function() {
+  //   var date = new Date('01-23-2017');
+  //   jasmine.clock().mockDate(date);
+  //   expect(transaction.formatDate()).toEqual('23/01/2017');
+  // });
 
   it('has a new balance property', function() {
     expect('_newBalance' in transaction).toBe(true);
--- src/transaction.js
+++ src/transaction.js
@@ -1,16 +1,17 @@
-function Transaction(type, amount, newBalance) {
-  this._type = type;
-  this._amount = amount.toFixed(2);
+// responsible for: adding and withdrawing funds
+function Transaction(amountCredited, amountDebited, newBalance) {
   this._date = new Date;
-  this._newBalance = newBalance.toFixed(2);
+  this._amountCredited = amountCredited;
+  this._amountDebited = amountDebited;
+  this._newBalance = newBalance;
 
-  Transaction.prototype.formatDate = function() {
-    date = this._date
-    day = '' + date.getDate(),
-    month = '' + (date.getMonth() + 1),
-    year = date.getFullYear();
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-    return [day, month, year].join('/');
-  };
+  // Transaction.prototype.formatDate = function() {
+  //   date = this._date
+  //   day = '' + date.getDate(),
+  //   month = '' + (date.getMonth() + 1),
+  //   year = date.getFullYear();
+  //   if (month.length < 2) month = '0' + month;
+  //   if (day.length < 2) day = '0' + day;
+  //   return [day, month, year].join('/');
+  // };
 };
