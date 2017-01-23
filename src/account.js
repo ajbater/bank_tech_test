29bad00a7273719b1118100773e888343ccf1528
ajbater <amanda22@hotmail.co.uk>
2017-01-23 20:25:48 +0000

    created methods for credit and debit that I would intend to be private methods

--- src/account.js
+++ src/account.js
@@ -5,15 +5,23 @@ function Account() {
 };
 
 Account.prototype.deposit = function(amount) {
-  this._balance += amount;
+  this.credit(amount);
   newBalance = this._balance;
   transaction = new Transaction(amount, null, newBalance);
   this._statement._transactionHistory.push(transaction);
 };
 
 Account.prototype.withdraw = function(amount) {
-  this._balance -= amount;
+  this.debit(amount);
   newBalance = this._balance;
   transaction = new Transaction(null, amount, newBalance);
   this._statement._transactionHistory.push(transaction);
+};
+
+Account.prototype.credit = function(amount) {
+  this._balance += amount;
+};
+
+Account.prototype.debit = function(amount) {
+  this._balance -= amount;
 };
