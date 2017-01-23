cc7bfbad6f0a80319fd0b08635d6fe642bd823ce
ajbater <amanda22@hotmail.co.uk>
2017-01-23 11:06:13 +0000

    removed a redundant test

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -11,10 +11,6 @@ describe('Transaction', function() {
     expect('_type' in transaction).toBe(true);
   });
 
-  it('has an amount, which will be be set upon instantiation', function() {
-    expect('_amount' in transaction).toBe(true);
-  });
-
   it('has a date, which will be a date object', function() {
     var newTransaction = new Transaction('withdraw', 500, '10-01-2012');
     expect(newTransaction._date).toEqual(jasmine.any(Date));
