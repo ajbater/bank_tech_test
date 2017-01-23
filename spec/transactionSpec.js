ddd6db1bf66babc14030522af2cf90a61032be38
ajbater <amanda22@hotmail.co.uk>
2017-01-23 11:10:47 +0000

    remove repitition in tests

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -1,24 +1,21 @@
 'use strict';
 
 describe('Transaction', function() {
-  var transaction;
+  var newTransaction;
 
   beforeEach(function() {
-    transaction = new Transaction();
+    newTransaction = new Transaction('withdraw', 500, '10-01-2012');
   });
 
   it('has a type, which will be passed upon instantiation', function() {
-    var newTransaction = new Transaction('withdraw', 500, '10-01-2012');
     expect(newTransaction._type).toEqual('withdraw');
   });
 
   it('has an amount, which will be an integer', function() {
-    var newTransaction = new Transaction('withdraw', 500, '10-01-2012');
     expect(newTransaction._amount).toEqual(500);
   });
 
   it('has a date, which will be a date object', function() {
-    var newTransaction = new Transaction('withdraw', 500, '10-01-2012');
     expect(newTransaction._date).toEqual(jasmine.any(Date));
   });
 });
